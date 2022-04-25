078f255cadd838d6852b1e1f114b6755d04cb84e
Gadzzaa <robert.popescu10@outlook.com>
2022-04-25 16:41:33 +0300

    Code cleaned, reformatted and refractored

--- Program.cs
+++ Program.cs
@@ -10,33 +10,24 @@ using OsuMemoryDataProvider.OsuMemoryModels;
 namespace MinimizeAppSomething
 {
     internal static class Program
-    {        
-        private static int _readDelay = 33;
-        
+    {
+        private static readonly int _readDelay = 33;
+
         private static readonly CancellationTokenSource Cts = new CancellationTokenSource();
         private static StructuredOsuMemoryReader _sreader;
         private static readonly OsuBaseAddresses BaseAddresses = new OsuBaseAddresses();
         private static OsuMemoryStatus _lastStatus;
 
-        private enum ShowWindowEnum
-        {
-            Hide = 0,
-            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
-            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
-            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
-            Restore = 9, ShowDefault = 10, ForceMinimized = 11
-        };
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")] private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        [DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);
 
         [STAThread]
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             _sreader = StructuredOsuMemoryReader.Instance.GetInstanceForWindowTitleHint(args.FirstOrDefault());
-            _sreader.InvalidRead += SreaderOnInvalidRead; 
-            await OsuData();                    
+            _sreader.InvalidRead += SreaderOnInvalidRead;
+            await OsuData();
         }
 
         private static async Task OsuData()
@@ -55,10 +46,13 @@ namespace MinimizeAppSomething
                         await Task.Delay(_readDelay);
                         continue;
                     }
+
                     _sreader.TryRead(BaseAddresses.GeneralData);
-                    if (BaseAddresses.GeneralData.OsuStatus == OsuMemoryStatus.Playing && _lastStatus != OsuMemoryStatus.Playing)
+                    if (BaseAddresses.GeneralData.OsuStatus == OsuMemoryStatus.Playing &&
+                        _lastStatus != OsuMemoryStatus.Playing)
                         MinimizeProgram();
-                    else if(BaseAddresses.GeneralData.OsuStatus != OsuMemoryStatus.Playing && _lastStatus == OsuMemoryStatus.Playing)
+                    else if (BaseAddresses.GeneralData.OsuStatus != OsuMemoryStatus.Playing &&
+                             _lastStatus == OsuMemoryStatus.Playing)
                         MaximizeProgram();
                     _lastStatus = BaseAddresses.GeneralData.OsuStatus;
                 }
@@ -67,24 +61,19 @@ namespace MinimizeAppSomething
 
         private static void MinimizeProgram()
         {
-            var p = System.Diagnostics.Process.GetProcessesByName("obs64").FirstOrDefault();
+            var p = Process.GetProcessesByName("obs64").FirstOrDefault();
             var p2 = Process.GetProcessesByName("osu!").FirstOrDefault();
-            if(p!=null)
-            {
-                ShowWindow(p.MainWindowHandle, (int)ShowWindowEnum.ShowMinimized);
-            }
+            if (p != null) ShowWindow(p.MainWindowHandle, (int) ShowWindowEnum.ShowMinimized);
         }
 
         private static void MaximizeProgram()
         {
-            var p = System.Diagnostics.Process.GetProcessesByName("obs64").FirstOrDefault();
+            var p = Process.GetProcessesByName("obs64").FirstOrDefault();
             var p2 = Process.GetProcessesByName("osu!").FirstOrDefault();
 
-            if(p!=null)
-            {
-                ShowWindow(p.MainWindowHandle, (int)ShowWindowEnum.ShowNormalNoActivate);
-            }
+            if (p != null) ShowWindow(p.MainWindowHandle, (int) ShowWindowEnum.ShowNormalNoActivate);
         }
+
         private static void SreaderOnInvalidRead(object sender, (object readObject, string propPath) e)
         {
             try
@@ -93,8 +82,24 @@ namespace MinimizeAppSomething
             }
             catch (ObjectDisposedException)
             {
+            }
+        }
 
-            }        
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1,
+            ShowMinimized = 2,
+            ShowMaximized = 3,
+            Maximize = 3,
+            ShowNormalNoActivate = 4,
+            Show = 5,
+            Minimize = 6,
+            ShowMinNoActivate = 7,
+            ShowNoActivate = 8,
+            Restore = 9,
+            ShowDefault = 10,
+            ForceMinimized = 11
         }
     }
 }
