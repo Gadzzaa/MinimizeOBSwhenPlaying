38514cfa6652985205e87c16fbc0f7f4b709115d
Gadzzaa <popescurobert986@gmail.com>
2023-03-20 09:12:35 +0200

    Gamma requirements met are done, need to adjust the gama through the console

--- Gamma.cs
+++ Gamma.cs
@@ -1,15 +1,18 @@
-﻿namespace OsuHG
+﻿using OsuMemoryDataProvider;
+
+namespace OsuHG
 {
     public class GammaProgram
     {
         private static GammaLibrary _gamma;
         public static void Gamma()
         {
+            if (Program.GeneralData.OsuStatus != OsuMemoryStatus.Playing) return;
            // _gamma = new GammaLibrary();
-          //  if(Program.BaseAddresses.Beatmap.Ar > 10.1)
+           if(Program.BaseAddresses.Beatmap.Ar > 10.1)
                 GammaLibrary.Set((float)Settings1.Default.highGamma);
-          //  if (Program.BaseAddresses.Beatmap.Ar < 9)
-          //      GammaLibrary.Set((float)Settings1.Default.lowGamma);
+           if (Program.BaseAddresses.Beatmap.Ar < 9)
+                GammaLibrary.Set((float)Settings1.Default.lowGamma);
         }
     }
 }
